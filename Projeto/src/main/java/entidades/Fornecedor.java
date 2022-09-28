2c603e8d102a9ddf016f46f779ae7f5400ab683c
Marcos-Goncalves <kirottow17@gmail.com>
2022-09-27 23:21:25 -0300

    feat: implementando cadastro de usuario, cliente, fornecedor e produto

---
+++ Projeto/src/main/java/entidades/Fornecedor.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author malxg
+ */
+public class Fornecedor {
+    private String nome;
+    private String cnpj;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return nome; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+    
+    
+}
